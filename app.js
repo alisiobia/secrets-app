da788ecdf1be89c886689eae2032ce4e560307bd
amarachi eze <alisiobia@gmail.com>
2021-11-19 08:22:06 +0100

    Upgraded security from md5 to bcrypt hash plus 10rounds salting

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 //const encrypt = require('mongoose-encryption'); //now elevating to hash md-5 instead of encryption
-const md5 = require('md5');
+//const md5 = require('md5'); // for simple hashing
+const bcrypt = require("bcrypt"); // for even better security
+const saltRounds = 10; // bcrypt salt round numbers for security
 
 
 
@@ -48,25 +50,30 @@ app.get("/login", function(req, res) {
 app.post("/login", function(req, res) {
   //at this point, search and confirm user exists then allow.
   const username = req.body.username;
-  const password = md5(req.body.password);//for hashing. normal leave as just req
+  const password = req.body.password;
+  //const password = md5(req.body.password);//for hashing. normal leave as just req
+
+
 
     User.findOne({email: username}, function(err, foundUser){
       if (err){
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets")
-          } else {
-            res.send("Incorrect password. Try again")
-          }
+          // Load hash from your password DB.
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                   // result == true
+                   if (result === true) {
+                     res.render("secrets")
+                   } else {
+                     res.send("Incorrect password. Try again")
+                   }
+                });
         } else {
-          res.send("User does not exist. Please register")
+            res.send("User does not exist. Please register")
         }
-      }
-
-    });
-
+      };//if no error
+    });//find login
     });//post login
 
 
@@ -75,21 +82,31 @@ app.get("/register", function(req, res) {
 });//get register
 
 app.post("/register", function(req, res) {
-  //at this point, add user to database
-  const newUser = new User ({
-     email:req.body.username,
-     //password: req.body.password, - normal or encryption security
-     password: md5(req.body.password) // hashing security
+//add bycrypt hash for good security
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      //at this point, add user to database
+      const newUser = new User ({
+         email:req.body.username,
+         //password: req.body.password, - normal or encryption security
+         //password: md5(req.body.password) // hashing security
+         password: hash // for hash encryption
 
-   }); //new user
+       }); //new user
 
-    newUser.save(function(err){
-     if (!err) {
-       res.render("secrets")
-     } else {
-       console.log(err);
-     }
-  })//save user
+        newUser.save(function(err){
+         if (!err) {
+           res.render("secrets")
+         } else {
+           console.log(err);
+         }
+      })//save user
+  });
+
+
+
+
+
 });//post register
 
 
